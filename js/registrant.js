e5326c476c2d38176832c804a72b0f468c9bbd41
Max <maksplugov@mail.ru>
2023-08-31 17:48:18 +0600

    add

--- js/registrant.js
+++ js/registrant.js
@@ -1,10 +1,13 @@
-
 let button = document.querySelector('.button-input-area');
 let number = document.querySelector('.number');
 let counter = 0;
 
 button.onclick = function () {
-  counter++;
-  number.textContent = counter;
+  if (button.classList.contains('added')) {
+    number.textContent--;
+  } else {
+    number.textContent++;
+  }
   button.classList.toggle('added');
+
 };
